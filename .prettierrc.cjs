6066974dc5b5502612b8fca4689071dc14f3bbbf
Mateusz Eiding <eidingmateusz@gmail.com>
2023-09-19 20:05:15 +0200

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'strict',
+    singleAttributePerLine: true,
+};
